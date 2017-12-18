5315b9cf8417818c03c8d6757fdd7494a5c40d9f
Piotr Murach <pmurach@gmail.com>
2017-12-18 17:10:34 +0100

    Change to break up letters

--- examples/doom.rb
+++ examples/doom.rb
@@ -6,9 +6,11 @@ font = TTY::Font.new(:doom)
 
 puts pastel.yellow(font.write("DOOM"))
 
-puts font.write("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+puts font.write("ABCDEFGHIJKLMN")
+puts font.write("OPQRSTUVWXYZ")
 
-puts font.write("abcdefghijklmnopqrstuvwxyz")
+puts font.write("abcdefghijklmn")
+puts font.write("opqrstuvwxyz")
 
 puts font.write("0123456789")
 
