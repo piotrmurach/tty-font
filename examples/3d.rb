8f9565c39ab5f27d044153b5352981d705265b03
Piotr Murach <pmurach@gmail.com>
2017-12-18 16:11:05 +0100

    Add 3d font example

---
+++ examples/3d.rb
@@ -0,0 +1,19 @@
+require 'tty-font'
+require 'pastel'
+
+pastel = Pastel.new
+font = TTY::Font.new("3d")
+
+puts pastel.green(font.write('3d'))
+
+puts font.write("ABCDEFGHIJKLMN")
+puts font.write("OPQRSTUVWXYZ")
+
+puts font.write("abcdefghijklmn")
+puts font.write("opqrstuvwxyz")
+
+puts font.write("0123456789")
+
+puts font.write("!#\$%^&*()-_+=")
+
+puts font.write("\"',.:;><?@][\\/}{|")
