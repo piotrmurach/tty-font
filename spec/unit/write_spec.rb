5c5164741effeb7de64315cc44b9b115463f3597
Piotr Murach <pmurach@gmail.com>
2022-04-24 21:37:09 +0200

    Fix standard font display of space k ~ characters

--- spec/unit/write_spec.rb
+++ spec/unit/write_spec.rb
@@ -1,6 +1,19 @@
 # frozen_string_literal: true
 
 RSpec.describe TTY::Font, "#write" do
+  let(:ascii_chars) { (32..126).map(&:chr).join }
+
+  {
+    "standard" => 6
+  }.each do |name, height|
+    it "writes supported ASCII characters in '#{name}' font" do
+      font = described_class.new(name)
+      output = font.write(ascii_chars)
+
+      expect(output.lines.size).to eq(height)
+    end
+  end
+
   it "writes lowercase letters in 'standard' font" do
     font = described_class.new
     expect(font.write("abcde")).to eq([
@@ -52,12 +65,12 @@ RSpec.describe TTY::Font, "#write" do
   it "writes text with space character in 'standard' font" do
     font = described_class.new
     expect(font.write("a b c d e")).to eq([
-      "           _                    _         ",
-      "   __ _   | |__      ___     __| |    ___ ",
-      "  / _` |  | '_ \\    / __|   / _` |   / _ \\",
-      " | (_| |  | |_) |  | (__   | (_| |  |  __/",
-      "  \\__,_|  |_.__/    \\___|   \\__,_|   \\___|",
-      "                                          "
+      "            _                      _          ",
+      "   __ _    | |__       ___      __| |     ___ ",
+      "  / _` |   | '_ \\     / __|    / _` |    / _ \\",
+      " | (_| |   | |_) |   | (__    | (_| |   |  __/",
+      "  \\__,_|   |_.__/     \\___|    \\__,_|    \\___|",
+      "                                              "
     ].join("\n"))
   end
 
@@ -88,12 +101,12 @@ RSpec.describe TTY::Font, "#write" do
   it "writes text with space characters and :letter_spacing set" do
     font = described_class.new
     expect(font.write("ab cd e", letter_spacing: 2)).to eq([
-      "            _                       _           ",
-      "   __ _    | |__        ___      __| |      ___ ",
-      "  / _` |   | '_ \\      / __|    / _` |     / _ \\",
-      " | (_| |   | |_) |    | (__    | (_| |    |  __/",
-      "  \\__,_|   |_.__/      \\___|    \\__,_|     \\___|",
-      "                                                "
+      "            _                        _            ",
+      "   __ _    | |__         ___      __| |       ___ ",
+      "  / _` |   | '_ \\       / __|    / _` |      / _ \\",
+      " | (_| |   | |_) |     | (__    | (_| |     |  __/",
+      "  \\__,_|   |_.__/       \\___|    \\__,_|      \\___|",
+      "                                                  "
     ].join("\n"))
   end
 
